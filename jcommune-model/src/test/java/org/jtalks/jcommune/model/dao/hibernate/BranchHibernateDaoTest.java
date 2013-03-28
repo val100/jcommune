a413a6f9f0f0b0e9baa562a42a74ea55ece7133c
senleft <alexandreteterin@gmail.com>
2013-03-28 18:00:00 +0400

    #JC-1258 Unit test fixed.

--- jcommune-model/src/test/java/org/jtalks/jcommune/model/dao/hibernate/BranchHibernateDaoTest.java
+++ jcommune-model/src/test/java/org/jtalks/jcommune/model/dao/hibernate/BranchHibernateDaoTest.java
@@ -81,11 +81,11 @@ public class BranchHibernateDaoTest extends AbstractTransactionalTestNGSpringCon
         assertReflectionEquals(branch, result);
     }
 
-    @Test(enabled = false)
-    public void testSaveWithTopic() {
-        JCUser topicStarter = ObjectsFactory.getDefaultUser();
-        Branch branch = ObjectsFactory.getDefaultBranch();
-        Topic topic = ObjectsFactory.getDefaultTopic();
+    @Test
+    public void testSaveBranchWithTopic() {
+        Branch branch = new Branch("name", "description");
+        JCUser topicStarter = new JCUser("username", "email@mail.com", "password");
+        Topic topic = new Topic(topicStarter, "title");
         branch.addTopic(topic);
         session.save(topicStarter);
         session.save(branch);
@@ -93,11 +93,13 @@ public class BranchHibernateDaoTest extends AbstractTransactionalTestNGSpringCon
         session.flush();
         assertEquals(getCount("select count(*) from org.jtalks.jcommune.model.entity.Branch"), 1);
         assertEquals(getCount("select count(*) from Topic"), 1);
+        assertEquals(getCount("select count(*) from User"), 1);
 
         branch.deleteTopic(topic);
         dao.update(branch);
         session.flush();
 
+        assertEquals(getCount("select count(*) from User"), 1);
         assertEquals(getCount("select count(*) from org.jtalks.jcommune.model.entity.Branch"), 1);
         assertEquals(getCount("select count(*) from Topic"), 0);
     }
